ae8c92d92c7890cca2b071b695ec13a4c2d34b8b
Tom Brodhead <tbrodhead@2u.com>
2022-06-13 11:47:35 -0500

    19.2.5 finished

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -4,6 +4,9 @@ import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 
+// Import CSS files
+import "../css/index.css";
+
 window.addEventListener("load", function () {
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
