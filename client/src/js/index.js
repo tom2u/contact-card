17d452e8be38166bc6bc3b439e66f6cef852268b
Tom Brodhead <tbrodhead@2u.com>
2022-06-17 11:05:34 -0500

    feature/19.2.8

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,14 +1,14 @@
 import "./form";
 import "./submit";
-import Logo from "../images/logo.png";
-import Bear from "../images/bear.png";
-import Dog from "../images/dog.png";
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Import CSS files
 import "../css/index.css";
 
-import { Tooltip, Toast, Popover } from "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Logo from "../images/logo.png";
+import Bear from "../images/bear.png";
+import Dog from "../images/dog.png";
 
 window.addEventListener("load", function () {
   document.getElementById("logo").src = Logo;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static("../client/"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("../client/dist/"));
+
 require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
